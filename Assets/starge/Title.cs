8714838e1066cf62540014996f438790beef34a3
Otsu01 <kudo.nonoka02@gmail.com>
2022-05-26 14:00:51 +0900

    最初のコミット

---
+++ Assets/starge/Title.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    private bool firstPush = false;
+    //startbutton
+    public void PressStart()
+    {
+        Debug.Log("Start!");
+
+        if(!firstPush)
+        {
+            Debug.Log("Go Next Scene!");
+            firstPush = true;
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
---
+++ Assets/starge/wallmove2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallmove2 : MonoBehaviour
+{
+    private float move = 0.03f;
+    public GameObject timeUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeUp = GameObject.Find("GameObject");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeUp.GetComponent<CountDouw>().isTimeUp == false)
+        {
+            Vector3 p = new Vector3(move, 0,0);
+            transform.Translate(p);
+        }
+    }
+}

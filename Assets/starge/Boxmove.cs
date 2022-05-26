25c5890d532d977febbbaa7222e564b3dbb6ded0
Otsu01 <kudo.nonoka02@gmail.com>
2022-05-26 13:46:54 +0900

    ignor

---
+++ Assets/starge/Boxmove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxmove : MonoBehaviour
+{
+
+    public float moveState;
+    public float speed;
+    Vector3 vector;
+
+    void Start()
+    {
+        vector = transform.position;
+    }
+
+
+    void Update()
+    {       
+        float x = moveState * Mathf.Sin(Time.time * speed);
+        transform.position = vector + new Vector3(x, 0, 0);
+    }
+}
+
---
+++ Assets/starge/GameClear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameClear : MonoBehaviour
+{
+    public GameObject GameC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GameClear")
+        {
+            Destroy(this.gameObject);
+            GameC.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+
+}
